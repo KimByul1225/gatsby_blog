b76400b5cdbe61b25647296d13d939843d70c1e6
ByulKim <kimbyul1225@gmail.com>
2023-06-27 19:29:35 +0900

    gatsby blog 프로젝트 시작

---
+++ src/components/Layout.tsx
@@ -0,0 +1,36 @@
+import React from 'react';
+import { Link } from "gatsby";
+
+
+interface ILayoutProps{
+    children: any;
+    title: string;
+}
+
+/**
+ * @description layout components nav와 footer를 포함하고 있음
+ */
+
+function Layout({children, title}: ILayoutProps) {
+    return (
+        <div className="container">
+            <nav>
+                <ul>
+                    <li><Link to="/">Home</Link></li>
+                    <li><Link to="/blog">Blog</Link></li>
+                </ul>
+            </nav>
+            
+            <main>
+                <h1>{title}</h1>
+                {children}
+            </main>
+            <footer>
+                footer
+            </footer>
+        </div>
+    );
+}
+
+export default Layout;
+
---
+++ src/pages/blog/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Layout from '../../components/Layout';
+
+function blog() {
+    return (
+        <Layout title="Blog">
+            <p>blog text</p>
+        </Layout>
+    );
+}
+
+export default blog;
