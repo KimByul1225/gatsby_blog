861ac3b5b7e688dc70f401c5c31b4f8d8c5314e1
ByulKim <kimbyul1225@gmail.com>
2023-06-28 23:18:29 +0900

    메인페이지 stack영역 작업중

--- src/pages/blog/index.tsx
+++ src/pages/blog/index.tsx
@@ -2,54 +2,15 @@ import React from 'react';
 import Layout from '../../components/Layout';
 import Seo from '../../components/Seo';
 
-import { graphql, PageProps } from 'gatsby';
 
-import Mtest from '../../../stack/index.mdx';
-
-import { MDXProvider} from "@mdx-js/react";
-
-
-
-const MyH1 = (props: React.JSX.IntrinsicAttributes & React.ClassAttributes<HTMLHeadingElement> & React.HTMLAttributes<HTMLHeadingElement>) => <h1 style={{ color: `tomato` }} {...props} />
-const MyParagraph = (props: React.JSX.IntrinsicAttributes & React.ClassAttributes<HTMLParagraphElement> & React.HTMLAttributes<HTMLParagraphElement>) => (
-    <p style={{ fontSize: "18px", lineHeight: 1.6 }} {...props} />
-)
-
-const components = {
-    h1: MyH1,
-    p: MyParagraph,
-}
-
-export default function blog({data}: PageProps<Queries.StackQuery>) {
-    
-    console.log("stack", data.allMdx.nodes[0].body);
-
-
-
+export default function blog() {
     return (
         <Layout title="Blog">
             <p>blog text</p>
-            <MDXProvider>
-                <Mtest/>
-            </MDXProvider>
-            {/* <MDXProvider>
-                {data.allMdx.nodes[0].body}
-            </MDXProvider> */}
         </Layout>
     );
 }
 
-export const query = graphql`
-    query Stack {
-        allMdx(filter: {frontmatter: {slug: {eq: "st"}}}) {
-            nodes{
-                body
-            }
-        }
-    }
-`
 
 export const Head = ()=> <Seo title="Blog"/>
 
-
-// node => node.fields.source === 'blog'
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,6 +3,20 @@ import Layout from "../components/Layout";
 import Seo from "../components/Seo";
 import { graphql, Link, PageProps } from 'gatsby';
 
+import Stack from "../../stack/index.mdx";
+
+import { MDXProvider} from "@mdx-js/react";
+
+// const MyH1 = (props: React.JSX.IntrinsicAttributes & React.ClassAttributes<HTMLHeadingElement> & React.HTMLAttributes<HTMLHeadingElement>) => <h1 style={{ color: `tomato` }} {...props} />
+// const MyParagraph = (props: React.JSX.IntrinsicAttributes & React.ClassAttributes<HTMLParagraphElement> & React.HTMLAttributes<HTMLParagraphElement>) => (
+//     <p style={{ fontSize: "18px", lineHeight: 1.6 }} {...props} />
+// )
+
+// const components = {
+//     h1: MyH1,
+//     p: MyParagraph,
+// }
+
 
 
 function WorkDetail({item}: {item: readonly (string | null)[] | null | undefined}) { 
@@ -21,8 +35,8 @@ function WorkDetail({item}: {item: readonly (string | null)[] | null | undefined
     )
 }
 
-export default function IndexPage({data}: PageProps<Queries.ExperienceQuery>) {
 
+export default function IndexPage({data}: PageProps<Queries.ExperienceQuery>) {
     console.log("!!!", data)
   return (
     <Layout title="메인페이지">
@@ -48,6 +62,11 @@ export default function IndexPage({data}: PageProps<Queries.ExperienceQuery>) {
               </div>
               ))
           }
+      </div>
+      <div>
+        <MDXProvider>
+          <Stack/>
+        </MDXProvider>
       </div>
     </Layout>
   )
