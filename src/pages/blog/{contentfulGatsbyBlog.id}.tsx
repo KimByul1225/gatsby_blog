f74dba58be44276c363135e6361ddd348e694cb4
ByulKim <kimbyul1225@gmail.com>
2023-07-01 00:43:19 +0900

    블로그 상세페이지 contentful rich text 영역 구현중

--- src/pages/blog/{contentfulGatsbyBlog.id}.tsx
+++ src/pages/blog/{contentfulGatsbyBlog.id}.tsx
@@ -3,27 +3,65 @@ import { GatsbyImage, getImage } from "gatsby-plugin-image";
 import React from "react";
 import Layout from "../../components/Layout";
 
+
+
+
+import { renderRichText } from "gatsby-source-contentful/rich-text";
+import { BLOCKS, MARKS } from "@contentful/rich-text-types";
+
+
 /**
  * @description Blog의 상세 내용을 보여주기 위한 components
  */
 
-export default function BlogDetail({data}: PageProps<Queries.BlogQuery>) {
+
+interface IBlogList {
+    contentfulGatsbyBlog: {
+        id: string;
+        title: string | null;
+        date: string | null;
+        category: string | null;
+        description: string | null;
+        detail: any;
+        headerImage : { readonly gatsbyImageData: import('gatsby-plugin-image').IGatsbyImageData | null, readonly file: { readonly url: string | null } | null };
+    }
+    
+}
+
+const options = {
+    renderMark: {
+        [MARKS.BOLD]: (text: string | number | boolean | React.ReactElement<any, string | React.JSXElementConstructor<any>> | Iterable<React.ReactNode> | React.ReactPortal | null | undefined) => <strong>{text}</strong>,
+    },
+    renderNode: {
+        [BLOCKS.PARAGRAPH]: (node: any, children: string | number | boolean | React.ReactElement<any, string | React.JSXElementConstructor<any>> | Iterable<React.ReactNode> | React.ReactPortal | null | undefined) => <p>{children}</p>,
+    },
+    renderText: (text: string) => {
+        if (!text) return <br />; 
+        return text.split("\n").flatMap((text, i) => [i > 0 && <br />, text]);
+    },
+    
+}
+
+
+
+export default function BlogDetail({data}: {data: IBlogList}) {
+    const { contentfulGatsbyBlog } = data;
+
+    console.log("contentfulGatsbyBlog", contentfulGatsbyBlog.detail.raw);
+
+    
     
     // const image = getImage(data.contentfulProduct?.preview?.gatsbyImageData!);
-    console.log("상세", data);
+    // console.log("상세", data);
     return (
-        // <Layout
-        //     title={data.contentfulProduct?.name!}
-        // >
-        //     <GatsbyImage
-        //         image={image!}
-        //         alt={data.contentfulProduct?.name!}
-        //     />
-        //     <h2>{data.contentfulProduct?.price}</h2>
-        // </Layout>
-        <div>
-            상세
-        </div>
+        <Layout
+            title="상세"
+        >
+            <div>상세</div>
+            {/* <div>{renderRichText(raw)}</div> */}
+            
+            <div>{renderRichText(contentfulGatsbyBlog.detail, options)}</div>
+        </Layout>
     )
 }
 export const query = graphql`
