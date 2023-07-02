1ff36acd519d9ef4e2dd6821bc4953ea58974329
ByulKim <kimbyul1225@gmail.com>
2023-07-02 19:36:15 +0900

    footer style작업 완료 header style작업 시작

--- src/components/Haeder.tsx
+++ src/components/Haeder.tsx
@@ -1,18 +1,71 @@
 import React from 'react';
 import { Link } from "gatsby";
+import { styled } from 'styled-components';
+import Row from './common/Row';
+
+import logo from "../images/common/logo_symbol.png"
 
 
 function Haeder() {
     return (
-        <nav>
-            <ul>
-                <li><Link to="/">Home</Link></li>
-                <li><Link to="/project">Project</Link></li>
-                <li><Link to="/blog">Blog</Link></li>
-            </ul>
-            <hr />
-        </nav>
+        <HeaderWrap>
+            <Row>
+                <SpaceBetween>
+                    <Logo
+                        to="/"
+                    >
+                        <h1>
+                            <span className="ir_so">
+                                홈으로
+                            </span>
+                        </h1>
+                    </Logo>
+
+                    <Navi>
+                        <ul>
+                            <li><Link to="/">Home</Link></li>
+                            <li><Link to="/project">Project</Link></li>
+                            <li><Link to="/blog">Blog</Link></li>
+                        </ul>
+                    </Navi>
+                </SpaceBetween>
+            </Row>
+        </HeaderWrap>
+        
     )
 }
 
 export default Haeder
+
+const HeaderWrap = styled.header`
+    padding: 50px 0;
+    border-bottom: 1px solid #000;
+`
+const SpaceBetween = styled.div`
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+`
+const Logo = styled(Link)`
+    display: inline-block;
+    h1{
+        width: 50px;
+        height: 50px;
+        background: url(${logo}) center no-repeat;
+        background-size: cover;
+    }
+`
+
+const Navi = styled.nav`
+    ul{
+        display: flex;
+        align-items: center;
+        justify-content: center;
+    }
+    li{
+        margin-right: 50px;
+    }
+    li:last-child{
+        margin-right: 0px;
+    }
+`
--- src/components/common/Row.tsx
+++ src/components/common/Row.tsx
@@ -13,8 +13,8 @@ export default Row;
 
 
 const Wrap = styled.div`
-    max-width: 1200px; margin: 0 auto;
-    @media screen and (max-width: 1200px){
+    max-width: 1000px; margin: 0 auto;
+    @media screen and (max-width: 1000px){
         max-width: 100%; padding: 0 20px;
     }
 `
