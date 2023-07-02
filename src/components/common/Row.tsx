ce2402494032a8043dad9d627e9e1b0986328660
ByulKim <kimbyul1225@gmail.com>
2023-07-02 18:39:19 +0900

    footer style작업 시작

---
+++ src/components/common/Row.tsx
@@ -0,0 +1,21 @@
+import React, { ReactNode } from 'react';
+import styled from 'styled-components';
+
+const Row = ({children}:{children: ReactNode}) => {
+    return (
+        <Wrap>
+            {children}
+        </Wrap>
+    );
+};
+
+export default Row;
+
+
+const Wrap = styled.div`
+    max-width: 1200px; margin: 0 auto;
+    @media screen and (max-width: 1200px){
+        max-width: 100%; padding: 0 20px;
+    }
+`
+
