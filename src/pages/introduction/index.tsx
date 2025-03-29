d5ae0de8584fb6321a9e5be73c2658017f63956d
ByulKim <byulkim1225@gmail.com>
2025-03-29 18:01:18 +0900

    feat: 각종 정보 변경

--- src/pages/introduction/index.tsx
+++ src/pages/introduction/index.tsx
@@ -1,10 +1,10 @@
 import * as React from "react";
 import Layout from "../../components/Layout";
 import Seo from "../../components/Seo";
-import { graphql, PageProps } from 'gatsby';
+import { graphql, PageProps } from "gatsby";
 import Stack from "../../../stack/index.mdx";
 import NowStudying from "../../../stack/nowStudying.mdx";
-import { MDXProvider} from "@mdx-js/react";
+import { MDXProvider } from "@mdx-js/react";
 import { styled } from "styled-components";
 import Row from "../../components/common/Row";
 import TagList from "../../components/common/TagList";
@@ -13,146 +13,144 @@ import TagList from "../../components/common/TagList";
  * @description introduction 페이지
  */
 
-export default function introduction({data}: PageProps<Queries.ExperienceQuery>) {
-    return (
-        <Layout>
-        <Row>
-            <SectionTitle>
-            Experience 🧑🏻‍💻
-            </SectionTitle>
-            <ExperienceWrap>
-            {
-                data.allMdx?.nodes.slice(0).reverse().map((item, index)=>(
-                <ExperienceList key={index}>
-                    <h3>
-                        {item.frontmatter?.companyName}
-                    </h3>
-                    <ExperienceDetail>
-                        <h4>{item.frontmatter?.task}</h4>
-                        <h5>{item.frontmatter?.team} / {item.frontmatter?.position}</h5>
-                        <p>
-                        {item.frontmatter?.startDate} ~ {item.frontmatter?.endDate && item.frontmatter?.endDate}
-                        {/* <span>{item.frontmatter?.period}</span> */}
-                        </p>
-                        <TagList item={item.frontmatter?.work}/>
-                    </ExperienceDetail>                
-                </ExperienceList>
-                ))
-            }
-            </ExperienceWrap>
-                    
-            <SectionTitle>
-            Stacks 💻
-            </SectionTitle>
-            <MdxWrap as="div">
-            <Stack/>
-            </MdxWrap>
-            
-            {/* <SectionTitle>
+export default function introduction({
+	data,
+}: PageProps<Queries.ExperienceQuery>) {
+	return (
+		<Layout>
+			<Row>
+				<SectionTitle>Experience 🧑🏻‍💻</SectionTitle>
+				<ExperienceWrap>
+					{data.allMdx?.nodes
+						.slice(0)
+						.reverse()
+						.map((item, index) => (
+							<ExperienceList key={index}>
+								<h3>{item.frontmatter?.companyName}</h3>
+								<ExperienceDetail>
+									<h4>{item.frontmatter?.task}</h4>
+									<h5>
+										{item.frontmatter?.team} /{" "}
+										{item.frontmatter?.position}
+									</h5>
+									<p>
+										{item.frontmatter?.startDate} ~{" "}
+										{item.frontmatter?.endDate &&
+											item.frontmatter?.endDate}
+										{/* <span>{item.frontmatter?.period}</span> */}
+									</p>
+									<TagList item={item.frontmatter?.work} />
+								</ExperienceDetail>
+							</ExperienceList>
+						))}
+				</ExperienceWrap>
+
+				<SectionTitle>Stacks 💻</SectionTitle>
+				<MdxWrap as="div">
+					<Stack />
+				</MdxWrap>
+
+				{/* <SectionTitle>
             Now Studying ✍🏻
             </SectionTitle>
             <MdxWrap as="div">
             <NowStudying/>
             </MdxWrap> */}
-        </Row>      
-        </Layout>
-    )
+			</Row>
+		</Layout>
+	);
 }
 
 export const query = graphql`
-    query Experience {
-        allMdx(filter: {frontmatter: {slug: {eq: "experience"}}}) {
-            nodes{
-                frontmatter {
-                companyName
-                endDate
-                period
-                position
-                slug
-                startDate
-                task
-                team
-                work
-                }
-            }
-        }
-    }
-`
+	query Experience {
+		allMdx(filter: { frontmatter: { slug: { eq: "experience" } } }) {
+			nodes {
+				frontmatter {
+					companyName
+					endDate
+					period
+					position
+					slug
+					startDate
+					task
+					team
+					work
+				}
+			}
+		}
+	}
+`;
 
-export const Head = ()=> <Seo title="Itroduction"/>
-
-
+export const Head = () => <Seo title="Itroduction" />;
 
 const SectionTitle = styled.h2`
-    font-size: 32px;
-    font-weight: 700;
-    border-left: 5px solid #000;
-    padding-left: 20px;
-    margin-top: 80px;
-    margin-bottom: 20px;
-    line-height: 45px;
-`
+	font-size: 32px;
+	font-weight: 700;
+	border-left: 5px solid #000;
+	padding-left: 20px;
+	margin-top: 80px;
+	margin-bottom: 20px;
+	line-height: 45px;
+`;
 
-const ExperienceWrap = styled.div`
-
-`
+const ExperienceWrap = styled.div``;
 
 const ExperienceList = styled.div`
-    padding: 30px 0;
-    display: flex;
-    align-items: center;
-    justify-content: space-between;
-    border-bottom: 1px solid #e4e4e4;
-    &:first-child{
-        border-top: 1px solid #e4e4e4;
-    }
-    h3{
-        width: 30%;
-        font-size: 28px;
-        font-weight: 600;
-    }
-    h4{
-        color: #ff4d15;
-        font-size: 18px;
-        margin-bottom: 20px;
-    }
-    h5{
-        margin-bottom: 10px;
-    }
-    p{
-        color: #818181;
-        font-weight: 300;
-        margin-bottom: 20px;
-        span{
-        color: #000;
-        display: inline-block;
-        margin-left: 10px;
-        font-weight: 600;
-        }
-    }
-    @media screen and (max-width: 768px){
-        flex-direction: column;
-        align-items: flex-start;
-        h3{
-            margin-bottom: 20px;
-            width: 100%;
-        }
-    }
-`
+	padding: 30px 0;
+	display: flex;
+	align-items: center;
+	justify-content: space-between;
+	border-bottom: 1px solid #e4e4e4;
+	&:first-child {
+		border-top: 1px solid #e4e4e4;
+	}
+	h3 {
+		width: 30%;
+		font-size: 28px;
+		font-weight: 600;
+	}
+	h4 {
+		color: #ff4d15;
+		font-size: 18px;
+		margin-bottom: 20px;
+	}
+	h5 {
+		margin-bottom: 10px;
+	}
+	p {
+		color: #818181;
+		font-weight: 300;
+		margin-bottom: 20px;
+		span {
+			color: #000;
+			display: inline-block;
+			margin-left: 10px;
+			font-weight: 600;
+		}
+	}
+	@media screen and (max-width: 768px) {
+		flex-direction: column;
+		align-items: flex-start;
+		h3 {
+			margin-bottom: 20px;
+			width: 100%;
+		}
+	}
+`;
 
 const ExperienceDetail = styled.div`
-    width: 70%;
-    @media screen and (max-width: 768px){
-        width: 100%;
-    }
-`
+	width: 70%;
+	@media screen and (max-width: 768px) {
+		width: 100%;
+	}
+`;
 
 const MdxWrap = styled(MDXProvider)`
-    font-size: 32px;
-    padding-top: 20px;
-    img{
-        height: 34px;
-        width: auto;
-        border-radius: 20px;
-    }
+	font-size: 32px;
+	padding-top: 20px;
+	img {
+		height: 34px;
+		width: auto;
+		border-radius: 20px;
+	}
 `;
